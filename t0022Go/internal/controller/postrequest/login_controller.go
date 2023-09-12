ffea6870ea1fd4dd34995b76c7098cb3a61a97eb
sharin-sushi <naruazcom230@gmail.com>
2023-09-13 02:25:03 +0900

    WIP:Go側でJWT実装。動作未確認だがpackage管理のため一度commit #1

--- t0022Go/internal/controller/postrequest/login_controller.go
+++ t0022Go/internal/controller/postrequest/login_controller.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/sharin-sushi/0022loginwithJWT/t0022Go/internal/controller/model"
+	"github.com/sharin-sushi/0022loginwithJWT/t0022Go/internal/controller/postrequest"
 	"github.com/sharin-sushi/0022loginwithJWT/t0022Go/internal/types"
 )
 
@@ -95,21 +96,24 @@ func PostLogin(c *gin.Context) {
 	}
 	// fmt.Printf("bitしたform=%v \n", form)
 
-	sessionID := generateSessionID()
-	fmt.Printf("sessionID=%v \n", sessionID)
-	c.SetCookie("LoginCookie", sessionID, 3600, "/", "localhost", false, true) //最後のとこ　HttpOnly trueならフロントで読み取れない
-	// POSTMANでCookieにセットされていること確認ok
 	fmt.Printf("bind内容:crsfToken=%v \n", form.csrfToken)
 
 	fmt.Printf("bind内容:ID=%v, name=%v, mail=%v, pass=%v, crat%v \n", form.MemberId, form.MemberName, form.Email, form.Password, form.CreatedAt)
 	//ここまでは処理確認ok
-	member, err := model.Login(form.MemberName, form.Password)
+	member, err := model.InquireIntoMember(form.MemberName, form.Password)
 	if err != nil {
 		c.Redirect(301, "/login")
 		return
 	}
 	fmt.Printf("DBから取得した情報=%+v \n ", member) //%vでも%sでも L1[GIN]
-	sessionManage(c, *member)
+
+	jwtToken := postrequest.GenerateToken(member.MemberId)
+
+	// sessionID := generateSessionID() →代わりにJWTtokenを使用
+	// sessionManage(c, *member)
+
+	c.SetCookie("loginToken", jwtToken, 3600, "/", "localhost", false, true) //最後のとこ　HttpOnly trueならフロントで読み取れない
+	// POSTMANでCookieにセットされていること確認ok
 
 	// フロント側の仕様
 	//サーバーがJSON形式のレスポンスを返し、その中に
@@ -117,11 +121,9 @@ func PostLogin(c *gin.Context) {
 	// true であれば認証成功
 
 	responseData := gin.H{
-		"status":     "ok",
-		"logined":    "authenticated",
-		"message":    "Login successful",
 		"memberId":   member.MemberId,
 		"memberName": member.MemberName,
+		"message":    "Login successful",
 	}
 	fmt.Println(responseData)
 	c.JSON(200, responseData)
@@ -135,6 +137,7 @@ func PostLogout(c *gin.Context) {
 	session.Save()
 }
 
+// JWTtoken使用により不要になった関数？
 //SessionManager.go　　セッション生成(alive, ID, 名前)
 func sessionManage(g *gin.Context, user types.Member) {
 	session := sessions.Default(g) //現在のセッションを取得
@@ -148,6 +151,7 @@ func sessionManage(g *gin.Context, user types.Member) {
 	}
 }
 
+// JWTtoken使用により不要になった関数？
 func generateSessionID() string {
 	b := make([]byte, 16) // 16バイトのランダムデータを生成
 	rand.Read(b)
@@ -155,62 +159,3 @@ func generateSessionID() string {
 	return hex.EncodeToString(b)
 
 }
-
-// sessionおくる…？　ChatGPTから教えてもらったけど、やってるこは現状とほぼ一緒なんだよな多分
-//  http.SetCookie(c.Writer, &cookie)は既現状にない？
-// func HandleLogin(c *gin.Context) {
-//     // セッションデータをセット
-//     SetSessionCookie(c, "memberID", user.MemberId)
-//     SetSessionCookie(c, "memberName", user.MemberName)
-//     // ログイン後の処理
-// }
-// func SetSessionCookie(c *gin.Context, key string, value string) {
-//     cookie := http.Cookie{
-//         Name:     key,
-//         Value:    value,
-//         Path:     "/",
-//         Domain:   "localhost", // ドメインを適切に設定
-//         MaxAge:   3600,        // クッキーの有効期限を設定（秒単位）
-//         HttpOnly: true,        // JavaScriptからアクセス不可にする→falseなら読み取れる？
-//     }
-//     http.SetCookie(c.Writer, &cookie)
-// }
-
-// //Cookie まだサイトのコピペ
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	// cookieの設定
-// 	expiration := time.Now()
-// 	expiration = expiration.AddDate(0, 0, 1)
-// 	cookie := http.Cookie{Name: "username", Value: "golang", Expires: expiration}
-// 	http.SetCookie(w, &cookie)
-// 	// クライアントからきたリクエストに埋め込まれているcookieの確認
-// 	for _, c := range r.Cookies() {
-// 		log.Print("Name:", c.Name, "Value:", c.Value)
-// 	}
-
-// 	if err := t.Execute(w, nil); err != nil {
-// 		log.Printf("failed to execute template: %v", err)
-// 	}
-// }
-
-// func EnvPass(c *gin.Context) {
-// 	q := c.Query("pass")
-// 	password := "L1"
-
-// 	//↑にハードコード
-// 	encryptPw1, _ := crypto.PasswordEncrypt(password)
-// 	// postmanでurlに記入http://localhost:8080/signup/envpass?pass=
-// 	encryptPw2, _ := crypto.PasswordEncrypt(q)
-
-// 	// if err1 != nil {
-// 	// 	fmt.Println("パスワード暗号化処理でエラー発生：", err1)
-// 	// 	return nil, err1
-// 	// }
-// 	// if err2 != nil {
-// 	// 	fmt.Println("パスワード暗号化処理でエラー発生：", err2)
-// 	// 	return nil, err2
-// 	// }
-// 	fmt.Printf("ハードコートでは%v → %v, \n", password, encryptPw1)
-// 	fmt.Printf("クエリのは %v → %v, \n", q, encryptPw2)
-
-// }
